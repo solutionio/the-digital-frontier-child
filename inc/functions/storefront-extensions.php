70d293ceafd3ec7057f2f09f413c06d4158d1db8
nevvkid <johannes.lohbihler@gmail.com>
2015-03-12 19:35:51 +0100

    removing boutique for new namespace 2

--- inc/functions/storefront-extensions.php
+++ inc/functions/storefront-extensions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Boutique Storefront extension compatibility
+ * digitalfrontier Storefront extension compatibility
  *
  * @package digitalfrontier
  */
@@ -9,7 +9,7 @@
  * Declare extensions this child theme doesn't support
  * @return  void
  */
-function boutique_extension_support() {
+function digitalfrontier_extension_support() {
 	add_filter( 'storefront_designer_enabled', '__return_false' );
 }
 add_action( 'init', 'digitalfrontier_extension_support', -1 );
